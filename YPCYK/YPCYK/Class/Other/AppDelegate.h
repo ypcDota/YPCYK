c70d3851df7c1abd380e7dd3dcdf2fa36f9eeb65
PCweibo <985348196@qq.com>
2017-02-23 12:16:26 +0800

    设置基本配置启动图片,项目分组

---
+++ YPCYK/YPCYK/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YPCYK
+//
+//  Created by ypc on 17/2/23.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
